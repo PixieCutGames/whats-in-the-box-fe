1e624a6d940bcb94a76ce355b29ca12ef807a874
Dina Abuzied <dina.abuzied@gmail.com>
2025-12-04 19:29:17 +0200

    added dark theme to item details

--- src/pages/Item/ItemDetails/ItemHeader.tsx
+++ src/pages/Item/ItemDetails/ItemHeader.tsx
@@ -1,13 +1,15 @@
 import { ArrowLeft, MoreVertical } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { Item } from "../../../types";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Menu, MenuButton } from "@headlessui/react";
 import { useState } from "react";
 import useItemActions from "../../../shared/hooks/useItemActions";
 import { useMediaQuery } from "@uidotdev/usehooks";
 import ConfirmationDialog from "../../../shared/components/ui/ConfirmationDialog";
 import AddEditDialog from "../../../shared/components/AddEditDialog";
 import toast from "react-hot-toast";
+import MenuItems from "../../../shared/components/ui/Menu/MenuItems";
+import MenuItemButton from "../../../shared/components/ui/Menu/MenuItemButton";
 
 type ItemHeaderProps = {
   item?: Item;
@@ -53,40 +55,29 @@ function ItemHeader({ item }: ItemHeaderProps) {
             e.preventDefault();
             navigate(-1);
           }}
-          className="p-2 -ml-2 hover:bg-background-accent rounded-lg transition-colors shrink-0"
+          className="p-2 -ml-2 hover:bg-background-accent dark:hover:bg-background-accent/10 rounded-lg transition-colors shrink-0"
         >
-          <ArrowLeft className="h-5 w-5 text-text-primary" />
+          <ArrowLeft className="h-5 w-5 text-text-primary dark:text-text-dark-primary" />
         </Link>
-        <h1 className="text-text-primary truncate">{item?.name}</h1>
+        <h1 className="text-text-primary dark:text-text-dark-primary truncate">
+          {item?.name}
+        </h1>
       </div>
 
       {/* Button group */}
       <div>
         <Menu>
-          <MenuButton className="p-2 hover:bg-background-accent rounded-lg transition-colors">
-            <MoreVertical className="h-5 w-5 text-text-primary" />
+          <MenuButton className="p-2 hover:bg-background-accent dark:hover:bg-background-accent/10 rounded-lg transition-colors">
+            <MoreVertical className="h-5 w-5 text-text-primary dark:text-text-dark-primary" />
           </MenuButton>
-          <MenuItems
-            anchor="bottom end"
-            className="mt-2 min-w-32 bg-popover text-popover-foreground border border-border rounded-md p-1 shadow-md focus:outline-none"
-          >
-            <MenuItem>
-              <button
-                onClick={onEdit}
-                className="block w-full rounded-sm px-2 py-1.5 text-sm text-start outline-hidden focus:bg-accent focus:text-accent-foreground text-text-primary hover:bg-background-accent hover:text-text-primary transition-colors no-underline"
-              >
-                Edit Item
-              </button>
-            </MenuItem>
-
-            <MenuItem>
-              <button
-                onClick={() => setOpenDeleteConfirmation(true)}
-                className="block w-full rounded-sm px-2 py-1.5 text-sm text-start outline-hidden text-destructive focus:bg-destructive/10 dark:focus:bg-destructive/20 focus:text-destructive hover:bg-destructive/10 dark:hover:bg-destructive/20 hover:text-destructive transition-colors no-underline"
-              >
-                Delete Item
-              </button>
-            </MenuItem>
+          <MenuItems anchor="bottom end" className="mt-2 min-w-32">
+            <MenuItemButton onClick={onEdit}>Edit Item</MenuItemButton>
+            <MenuItemButton
+              danger
+              onClick={() => setOpenDeleteConfirmation(true)}
+            >
+              Delete Box
+            </MenuItemButton>
           </MenuItems>
         </Menu>
       </div>
@@ -100,11 +91,11 @@ function ItemHeader({ item }: ItemHeaderProps) {
         onCancel={() => setOpenDeleteConfirmation(false)}
         onConfirm={onDelete}
       >
-        <p className="text-sm text-text-secondary sm:hidden">
+        <p className="text-sm text-text-secondary dark:text-text-dark-secondary sm:hidden">
           This will permanently delete this item.
         </p>
 
-        <p className="text-sm text-text-secondary sm:block hidden">
+        <p className="text-sm text-text-secondary dark:text-text-dark-secondary sm:block hidden">
           Are you sure you want to delete {item?.name}?
           <br />
           This action cannot be undone.
